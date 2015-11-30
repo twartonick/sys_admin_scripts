9de96671a93178a1be14cd8f3f090095bbcefa87
twartonick <thomaswartonick@gmail.com>
2015-11-30 12:08:34 -0700

    capture and alert high load on cpanel server
    capture and alert high load on cpanel server

---
+++ cpanel_load_high_dump_alert.sh
@@ -0,0 +1,177 @@
+#!/bin/sh
+
+export PATH=/bin:/usr/bin
+
+##########################################################################
+#                                                                        #
+# Code modifications commented below copyright Thomas Wartonick 2015     #
+# under same licence referenced below by original author.                #
+#                                                                        #
+##########################################################################
+##########################################################################
+#                                                                        #
+#  Copyright Jeff Petersen, 2009 - 2013                                  #
+#                                                                        #
+#  This program is free software: you can redistribute it and/or modify  #
+#  it under the terms of the GNU General Public License as published by  #
+#  the Free Software Foundation, either version 3 of the License, or     #
+#  (at your option) any later version.                                   #
+#                                                                        #
+#  This program is distributed in the hope that it will be useful,       #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+#  GNU General Public License for more details.                          #
+#                                                                        #
+#  You should have received a copy of the GNU General Public License     #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>. #
+#                                                                        #
+##########################################################################
+
+
+
+
+###############################################################################
+# START USER CONFIGURABLE VARIABLES
+###############################################################################
+
+EMAIL="your_email_addy"
+
+# 1 minute load avg
+MAX_LOAD=1
+
+# kB
+MAX_SWAP_USED=1000
+
+# kB
+MAX_MEM_USED=3795064
+
+# packets per second inbound
+MAX_PPS_IN=2000
+
+# packets per second outbound
+MAX_PPS_OUT=2000
+
+# max processes in the process list
+MAX_PROCS=400
+
+###############################################################################
+# END USER CONFIGURABLE VARIABLES
+###############################################################################
+
+
+IFACE=`grep ETHDEV /etc/wwwacct.conf | awk '{print $2}'`
+if [[ "$IFACE" =~ "venet" ]] ; then
+    IFACE=venet0
+fi
+
+IFACE=${IFACE}:
+
+###############################################################################
+# 1 min load avg
+###############################################################################
+ONE_MIN_LOADAVG=`cut -d . -f 1 /proc/loadavg`
+echo "1 minute load avg: $ONE_MIN_LOADAVG"
+
+
+###############################################################################
+# swap used
+###############################################################################
+SWAP_TOTAL=`grep ^SwapTotal: /proc/meminfo | awk '{print $2}'`
+SWAP_FREE=`grep ^SwapFree: /proc/meminfo | awk '{print $2}'`
+
+let "SWAP_USED = (SWAP_TOTAL - SWAP_FREE)"
+echo "Swap used: $SWAP_USED kB"
+
+
+###############################################################################
+# mem used
+###############################################################################
+MEM_TOTAL=`grep ^MemTotal: /proc/meminfo | awk '{print $2}'`
+MEM_FREE=`grep ^MemFree: /proc/meminfo | awk '{print $2}'`
+
+let "MEM_USED = (MEM_TOTAL - MEM_FREE)"
+echo "Mem used: $MEM_USED kB"
+
+
+###############################################################################
+# packets received
+###############################################################################
+PACKETS_RX_1=`grep $IFACE /proc/net/dev | awk '{print $2}'`
+sleep 2;
+PACKETS_RX_2=`grep $IFACE /proc/net/dev | awk '{print $2}'`
+
+let "PACKETS_RX = (PACKETS_RX_2 - PACKETS_RX_1) / 2"
+echo "packets received (2 secs): $PACKETS_RX"
+
+
+###############################################################################
+# packets sent
+###############################################################################
+PACKETS_TX_1=`grep $IFACE /proc/net/dev | awk '{print $10}'`
+sleep 2;
+PACKETS_TX_2=`grep $IFACE /proc/net/dev | awk '{print $10}'`
+
+let "PACKETS_TX = (PACKETS_TX_2 - PACKETS_TX_1) / 2"
+echo "packets sent (2 secs): $PACKETS_TX"
+
+let "SWAP_USED = SWAP_TOTAL - SWAP_FREE"
+if [ ! "$SWAP_USED" == 0 ] ; then
+    PERCENTAGE_SWAP_USED=`echo $SWAP_USED / $SWAP_TOTAL | bc -l`
+    TOTAL_PERCENTAGE=`echo ${PERCENTAGE_SWAP_USED:1:2}%`
+else
+    TOTAL_PERCENTAGE='0%'
+fi
+
+
+###############################################################################
+# number of processes
+###############################################################################
+MAX_PROCS_CHECK=`ps ax | wc -l`
+
+
+send_alert()
+
+###############################################################################
+# Customization/modification begins here.
+# 
+# I wanted more info documented at time of load issue 
+# for point in time root cause investigation:
+# server-status, mysql, netstat, top
+#
+###############################################################################
+
+{	
+		elinks -dump  http://your_domain/server-status >> /var/log/cpanel_load_high/server_status_`date +%Y%m%d%H%M`.log &
+
+        mysqladmin processlist >> /var/log/cpanel_load_high/mysql_processlist_`date +%Y%m%d%H%M`.log &
+
+        netstat -anp >> /var/log/cpanel_load_high/netstat_output_`date +%Y%m%d%H%M`.log &
+        wait
+
+        cd /var/log/cpanel_load_high
+
+        cat *`date +%Y%m%d%H%M`.log >> combined_log_`date +%Y%m%d%H%M`.log
+
+        top -n2 -c -b >> /var/log/cpanel_load_high/combined_log_`date +%Y%m%d%H%M`.log
+		
+# Modification ends here
+
+        SUBJECTLINE="`hostname` Problem see /var/log/cphost_load_high dir [L: $ONE_MIN_LOADAVG] [P: $MAX_PROCS_CHECK] [Swap Use: $TOTAL_PERCENTAGE ] [pps in: $PACKETS_RX pps out: $PACKETS_TX]"
+		top -n2 -c -b  |  mail -s "$SUBJECTLINE" $EMAIL
+		
+    exit
+	
+
+}
+
+
+if   [ $ONE_MIN_LOADAVG -gt $MAX_LOAD      ] ; then send_alert
+elif [ $SWAP_USED       -gt $MAX_SWAP_USED ] ; then send_alert
+elif [ $MEM_USED        -gt $MAX_MEM_USED  ] ; then send_alert
+elif [ $PACKETS_RX      -gt $MAX_PPS_IN    ] ; then send_alert
+elif [ $PACKETS_TX      -gt $MAX_PPS_OUT   ] ; then send_alert
+elif [ $MAX_PROCS_CHECK -gt $MAX_PROCS ] ; then send_alert
+fi
+
+
+
